8408d4576a58b5c411d6fa6ac47aad5c56ba3d3f
Javier Alvarez <neiker@gmail.com>
2017-06-04 14:21:20 -0300

    Refactor: move scraping logic to src/scraper.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,23 +6,9 @@ module.exports = {
   "env": {
     node: true,
     browser: true,
+    es6: true,
   },
   "rules": {
-    "no-use-before-define": 0,
-    "func-names": 0,
     "no-console": 0,
-    "comma-dangle": 0,
-    "no-underscore-dangle": 0,
-    "consistent-return": 0,
-    "no-cond-assign": 0,
-    "no-plusplus": 0,
-    "max-len": ["error", 140],
-    "newline-per-chained-call": 0,
-    "spaced-comment": 0,
-    "no-shadow": [
-      "error",
-      { "builtinGlobals": false, "hoist": "functions", "allow": ["err", "cb", "callback"] }
-    ],
-    "arrow-body-style": 0,
   },
 };
--- index.js
+++ index.js
@@ -2,161 +2,28 @@
 
 require('dotenv').config()
 
-const fs = require('fs');
-
 const { table } = require('table');
-const { ChromeLauncher } = require('lighthouse/lighthouse-cli/chrome-launcher');
-const chrome = require('chrome-remote-interface');
-
-let launcher;
-
-async function launchChrome() {
-  launcher = new ChromeLauncher({
-    port: 9222,
-    autoSelectChrome: true, // False to manually select which Chrome install.
-    additionalFlags: [
-      '--window-size=1280,1024',
-      '--disable-gpu',
-      '--headless',
-    ]
-  });
-
-  try {
-    await launcher.run();
-  } catch (e) {
-    await launcher.kill();
-    throw e;
-  }
-}
+const Scraper = require('./src/scraper.js');
 
 async function init() {
-  await launchChrome();
-  const client = await chrome();
+  const scraper = new Scraper();
 
-  client.on('error', (err) => {
-    throw Error('Cannot connect to Chrome: ', err);
-  });
-
-  const { Page, Runtime, DOM } = client;
-
-  const stage0 = () => {
-    console.log('Loading bbvafrances.com.ar...');
-    client.once('Page.loadEventFired', stage1);
-    Page.navigate({url: 'https://www.bbvafrances.com.ar/fnetcore/loginClementeApp.html'});
-  };
-
-  const stage1 = () => {
-    console.log('Loaded');
-    // ugly hack because of redirect...
-    client.once('Page.loadEventFired', () => {
-      client.once('Page.frameDetached', () => {
-        client.once('Page.loadEventFired', stage2);
-      });
-    });
-    login(client);
-  };
-
-  const stage2 = async () => {
-    console.log('Log in successful');
-    const { accounts, cards } = await productInfo(client);
-    console.log('Accounts:\n');
-    console.log(table(accounts));
-    console.log('Cards:\n');
-    console.log(table(cards));
-    //await takeScreenshot(client);
-    logout(client);
-  };
-
-  await Page.enable();
-  await Runtime.enable();
-  await DOM.enable();
-
-  stage0();
-}
-
-async function login(client) {
-  const formValues = {
+  await scraper.login({
     dni: process.env.BBVA_DNI,
     user: process.env.BBVA_USER,
     password: process.env.BBVA_PASSWORD,
-  };
-  const browserCode = ({ dni, user, password }) => {
-    return new Promise((resolve, reject) => {
-      const fillValues = () => {
-        document.querySelector('#documentNumberInput').value = dni;
-        document.querySelector('#documentNumberInput').dispatchEvent(new Event('input'));
-        document.querySelector('[name="digitalUser"]').value = user;
-        document.querySelector('[name="digitalUser"]').dispatchEvent(new Event('input'));
-        document.querySelector('[name="digitalKey"]').value = password;
-        document.querySelector('[name="digitalKey"]').dispatchEvent(new Event('input'));
-        document.querySelector('.btn.btn-success.btn-login-modal').click();
-        resolve();
-      };
-      setTimeout(fillValues, 100);
-    });
-  };
-
-  return client.Runtime.evaluate({
-    expression: `(${browserCode})(${JSON.stringify(formValues)})`,
-    awaitPromise: true,
   });
-}
 
-async function logout(client) {
-  console.log('Logging out...');
-  client.once('Page.frameDetached', async () => {
-    console.log('Logged out.');
-    await client.close();
-    launcher.kill(); // Kill Chrome.
-  });
-  const browserCode = () => {
-    document.querySelector('.btn-logout').click();
-  };
-  return client.Runtime.evaluate({
-    expression: `(${browserCode})()`,
-  });
-}
+  const { accounts, cards } = await scraper.getProductInfo();
 
-async function productInfo(client) {
-  console.log('Fetching accounts...');
-  const browserCode = () => {
-    const cleanup = (str) => {
-      return str.trim().replace(/\s+/g, ' ').replace(/\n/g, '');
-    };
-    const buildItems = (container) => {
-      const rows = Array.from(container.querySelectorAll('.only-balance table tr')).slice(1);
-      return rows.map((row) => {
-        return [
-          cleanup(row.querySelector('td:nth-child(1) div').textContent),
-          cleanup(row.querySelector('td:nth-child(3)').textContent),
-        ];
-      });
-    }
-    return new Promise((resolve, reject) => {
-      new MutationObserver((mutations, observer) => {
-        const containers = document.querySelectorAll('.bbva-tables');
-        if (!containers.length) return;
-        const accounts = buildItems(containers[0]);
-        const cards = buildItems(containers[1]);
-        observer.disconnect();
-        resolve({ accounts, cards });
-      }).observe(document.body, {
-        childList: true
-      });
-    });
-  };
+  console.log('Accounts:\n');
+  console.log(accounts.length ? table(accounts) : '- No accounts found -\n');
 
-  const result = await client.Runtime.evaluate({
-    expression: `(${browserCode})()`,
-    awaitPromise: true,
-    returnByValue: true,
-  });
-  return result.result.value;
-}
+  console.log('Cards:\n');
+  console.log(cards.length ? table(cards) : '- No cards found -\n');
 
-async function takeScreenshot(client) {
-  const { data } = await client.Page.captureScreenshot();
-  fs.writeFileSync('bbva.png', Buffer.from(data, 'base64'));
+  //await scraper.takeScreenshot();
+  await scraper.logout();
 }
 
 init();
